5e535a9e5597a3773a22c73ac2532145aea8c3f4
Melihcan Çilek <mcilek2018@gtu.edu.tr>
2022-11-02 19:11:42 +0300

    feat(paging) : paging done with queue approach

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
   const [username, setUsername] = useState("");
   const [data, setData] = useState([]);
+  const [pageQueue, setPageQueue] = useState([
+    {
+      firstID: 0,
+      lastID: 0,
+    },
+  ]);
+  const [page, setPage] = useState(0);
   const [loading, setLoading] = useState(false);
+
   const handleChangeUsername = (e) => {
     setUsername(e.target.value);
     console.log(username);
   };
 
-  useEffect(() => {
+  const fetchData = useCallback(
+    async (buttonID) => {
+      fetch(
+        `https://api.github.com/users?since=${
+          buttonID === `next`
+            ? pageQueue[page].lastID
+            : pageQueue[page - 1].firstID
+        }&&per_page=10`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data);
+          buttonID === `next` && page === pageQueue.length - 1
+            ? pageQueue[page].lastID === 0 && pageQueue[page].firstID === 0
+              ? setPageQueue([
+                  {
+                    firstID: pageQueue[page].lastID,
+                    lastID: data[data.length - 1].id,
+                  },
+                ])
+              : setPageQueue([
+                  ...pageQueue,
+                  {
+                    firstID: pageQueue[page].lastID,
+                    lastID: data[data.length - 1].id,
+                  },
+                ])
+            : setPageQueue([...pageQueue]);
+
+          setLoading(false);
+        });
+    },
+    [pageQueue, page]
+  );
+
+  const handlePagination = (e) => {
     setLoading(true);
-    fetch("https://api.github.com/users?per_page=100")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
+    const buttonID = e.currentTarget.id;
+    setPage(buttonID === "next" ? page + 1 : page - 1);
+    fetchData(buttonID);
+  };
+
+  useEffect(() => {
+    fetchData("next");
   }, []);
 
-  const searchInRepo = () => {};
+  const searchInRepo = () => {
+    // fetch(
+    //   `https://api.github.com/search/users?q=${username}&&page=${page}&&per_page=10`
+    // )
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     setData(data.items);
+    //   });
+  };
 
   return (
     <div className='page'>
@@ -34,17 +87,31 @@ function App() {
             onChange={handleChangeUsername}
           />
           <button className='button' type='button' onClick={searchInRepo}>
-            {loading ? "Searching..." : "Search"}
+            Search
           </button>
         </form>
+        <div className='pagination'>
+          {page > 0 && (
+            <button className='button' id='prev' onClick={handlePagination}>
+              Prev
+            </button>
+          )}
+          <button className='button' id='next' onClick={handlePagination}>
+            Next
+          </button>
+        </div>
         <div className='users'>
           {loading ? (
-            <h1>Loading...</h1>
+            <h1 className='user'>Loading...</h1>
           ) : (
             data.map((user) => {
               return (
                 <div className='user' key={user.avatar_url}>
-                  <img className="avatar-url" src={user.avatar_url} alt='user' />
+                  <img
+                    className='avatar-url'
+                    src={user.avatar_url}
+                    alt='user'
+                  />
                   <div className='user-info'>
                     <h2>{user.login}</h2>
                   </div>
