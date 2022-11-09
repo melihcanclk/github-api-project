190e28eaaf887a29ae199e04f25b45e06338b574
Melihcan Çilek <mcilek2018@gtu.edu.tr>
2022-11-09 10:55:29 +0300

    fix(error): unused variable and imports deletion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { useCallback, useEffect, useState } from "react";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
